fc3ae36af698c826849a354ca182349d931d4a4f
João Carlos Castro Zacchello <joaozacchello@xongs-macbook-air.local>
2025-07-27 23:57:41 -0300

    added jwt manager

--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import declarative_base
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy(model_class=declarative_base())
+jwt = JWTManager()
