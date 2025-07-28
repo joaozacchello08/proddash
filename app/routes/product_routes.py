8f15344d3d388b80673388118460614f55abe460
João Carlos Castro Zacchello <joaozacchello@xongs-macbook-air.local>
2025-07-28 01:07:13 -0300

    added a GET and POST method for products

--- app/routes/product_routes.py
+++ app/routes/product_routes.py
@@ -1,45 +1,50 @@
-from flask import Blueprint, request, abort, jsonify
+from flask import Blueprint, request, jsonify
 from app.extensions import db
-from app.models import Dashboard, Product
+from app.models import User, Dashboard, Product
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 
 product_bp = Blueprint("product_bp", __name__)
 
-#region products
-#region CREATE product
+#region create product
 @product_bp.route("/", methods=["POST"])
+@jwt_required()
 def create_product():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    if not user:
+        return jsonify({ "message": "User not found" }), 404
+    
+    dashboard = user.dashboard
+    if not dashboard:
+        return jsonify({ "message": "Couldn't find this user dashboard" }), 404
+    
     body = request.json
-
     if not body:
-        abort(400)
+        return jsonify({ "message": "No JSON found on request" }), 400
+    
+    productName = body.get("productName")
+    productImage = body.get("productImage")
+    productPrice = body.get("productPrice")
+    productCost = body.get("productCost")
+    productBarcode = body.get("productBarcode")
+    productStock = body.get("productStock")
 
-    dashboard_id = body.get("dashboardId")
-    product_name = body.get("productName")
-    product_price = body.get("productPrice")
-    product_stock = body.get("productStock", 0)
-    product_image = body.get("productImage")
-    product_barcode = body.get("productBarcode")
-    product_cost = body.get("productCost")
+    if not all([productName, productPrice]):
+        return jsonify({ "message": "Product name and price are required." }), 400
 
-    if not all([dashboard_id, product_name, product_price is not None]):
-        abort(400)
+    # TO-DO
+    # check if a product with the productName or barcode already exists in user's dashboard
 
     try:
-        dashboard = db.session.get(Dashboard, dashboard_id)
-        if not dashboard:
-            abort(404)
-        
-        if Product.query.filter_by(dashboardId=dashboard_id, productName=product_name).first() or Product.query.filter_by(dashboardId=dashboard_id, productBarcode=product_barcode).first():
-            abort(409)
-        
         new_product = Product(
-            productName=product_name,
-            productPrice=float(product_price),
-            productStock=int(product_stock),
-            productImage=product_image,
-            productBarcode=product_barcode,
-            productCost=float(product_cost) if product_cost is not None else None
+            dashboardId=dashboard.id,
+            productName=productName,
+            productImage=productImage,
+            productPrice=productPrice,
+            productCost=productCost,
+            productBarcode=productBarcode,
+            productStock=productStock
         )
 
         new_product.dashboard = dashboard
@@ -47,75 +52,21 @@ def create_product():
         db.session.add(new_product)
         db.session.commit()
     except Exception as e:
+        db.session.rollback()
         print(f"Error: {str(e)}")
-        abort(500)
-    
-    return jsonify({ "message": "Product created successfully!", "newProduct": new_product.serialize() }), 201
+        return jsonify({ "message": "Error creating product" }), 500
+
+    return jsonify({ "message": "Product registered successfully!", "newProduct": new_product.serialize() }), 201
 #endregion
 
-#region UPDATE product
-@product_bp.route("/", methods=["UPDATE"])
-def update_product():
-    body = request.json
-
-    if not body:
-        abort(400)
+#region read product
+@product_bp.route("/", methods=["GET"])
+@jwt_required()
+def get_products():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    if not user:
+        return jsonify({ "message": "User not found" }), 404
     
-    product_id = body.get("productId")
-    updates = body.get("updates")
-
-    if not all([product_id, updates]):
-        abort(400)
-
-    allowed_updates = ["productName", "productImage", "productPrice", "productCost", "productBarcode", "productStock"]
-
-    try:
-        product = db.session.get(Product, product_id)
-        if not product:
-            abort(404)
-
-        for update in updates:
-            for key, value in update.items():
-                if key in allowed_updates:
-                    setattr(product, key, value)
-        
-        db.session.commit()
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        abort(500)
     
-    return jsonify({ "updatedProduct": product.serialize() }), 200
-#endregion
-
-#region DELETE product
-@product_bp.route("/<int:product_id>", methods=["DELETE"])
-def delete_product(product_id: int):
-    try:
-        product = db.session.get(Product, product_id)
-        if not product:
-            abort(404)
-        db.session.delete(product)
-        db.session.commit()
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        abort(500)
-    
-    return jsonify({ "deletedProduct": product.serialzie(), "message": "Product deleted successfully!" }), 200
-#endregion
-#endregion
-
-#region READ
-@product_bp.route("/<int:product_id>", methods=["GET"])
-def get_product(product_id: int):
-    product = db.session.get(Product, product_id)
-    if not product:
-        abort(404)
-    return jsonify({ "product": product.serialize() }), 200
-
-@product_bp.route("/by-dashboard/<int:dashboard_id>", methods=["GET"])
-def get_dashboard_products(dashboard_id: int):
-    dashboard = db.session.get(Dashboard, dashboard_id)
-    if not dashboard:
-        abort(404)
-    return jsonify({ "products": [product.serialize() for product in dashboard.products] }), 200
 #endregion
