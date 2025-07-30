e625af40d133ec4e670aa535bb95f39a9ea84052
João Carlos Castro Zacchello <joaozacchello@xongs-macbook-air.local>
2025-07-30 01:06:28 -0300

    updated configs

--- config.py
+++ config.py
@@ -3,16 +3,24 @@ from os import getenv
 
 load_dotenv()
 
+USER = getenv("user")
+PASSWORD = getenv("password")
+HOST = getenv("host")
+PORT = getenv("port")
+DBNAME = getenv("dbname")
+
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = getenv("SECRET_KEY")
+    ALLOWED_IPS = getenv("ALLOWED_IPS").split(",")
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = getenv("DEVELOPMENT_SQLALCHEMY_DATABASE_URI")
-    ALLOWED_IPS = getenv("ALLOWED_IPS").split(",")
 
 class DeployDevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = getenv("DEPLOY_DEV_SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
 class ProductionConfig(Config):
     pass
