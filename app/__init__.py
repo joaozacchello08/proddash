845d65e7d058dafe96792ab5060db1a5405f0946
João Carlos Castro Zacchello <joaozacchello@xongs-macbook-air.local>
2025-07-30 00:53:03 -0300

    adding more security

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from .extensions import db, jwt
 from .routes import register_routes
@@ -9,15 +9,36 @@ def create_app():
     app = Flask(__name__) 
 
     flask_env = os.environ.get("FLASK_ENV", "development")
-    if flask_env == "production":
-        app.config.from_object("config.ProductionConfig")
-    else:
-        app.config.from_object("config.DevelopmentConfig")
+    match(flask_env):
+        case "development":
+            app.config.from_object("config.DevelopmentConfig")
+        
+        case "dev_deploy":
+            app.config.from_object("config.DeployDevConfig")
+
+        case "production":
+            app.config.from_object("config.ProductionConfig")
+
+        case _:
+            app.config.from_object("config.DevelopmentConfig")
 
     db.init_app(app)
     jwt.init_app(app)
     CORS(app)
     register_routes(app)
+
+    @app.before_request
+    def secure_requests():
+        allowed_ips = app.config["ALLOWED_IPS"]
+        if request.remote_addr not in allowed_ips:
+            return jsonify({ "message": "Unauthorized IP" }), 403
+        
+        if not request.is_secure and app.env == "production":
+            return jsonify({ "message": "Only HTTPS are accepted." }), 403
+        
+        api_key = request.headers.get("X-API-KEY")
+        if api_key != app.config["API_SECRET_KEY"]:
+            return jsonify({ "message": "API key invalid." }), 401 
 
     @jwt.token_in_blocklist_loader
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
--- config.py
+++ config.py
@@ -1,15 +1,18 @@
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
-config = dotenv_values()
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = config["SECRET_KEY"] if config["SECRET_KEY"] else getenv("SECRET_KEY")
+    SECRET_KEY = getenv("SECRET_KEY")
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = config["DEVELOPMENT_SQLALCHEMY_DATABASE_URI"] if config["DEVELOPMENT_SQLALCHEMY_DATABASE_URI"] else getenv("DEVELOPMENT_SQLALCHEMY_DATABASE_URI") 
+    SQLALCHEMY_DATABASE_URI = getenv("DEVELOPMENT_SQLALCHEMY_DATABASE_URI")
+    ALLOWED_IPS = getenv("ALLOWED_IPS").split(",")
+
+class DeployDevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = getenv("DEPLOY_DEV_SQLALCHEMY_DATABASE_URI")
 
 class ProductionConfig(Config):
     pass
