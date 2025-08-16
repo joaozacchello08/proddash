6c1e3e49867586cdf6f030066ac1e0c9a1abd7d0
João Carlos Castro Zacchello <joaozacchello@xongs-macbook-air.local>
2025-08-16 01:46:26 -0300

    fixed/upgraded config

--- config.py
+++ config.py
@@ -1,61 +1,18 @@
-from dotenv import load_dotenv
-from os import environ, getenv
-from getpass import getuser
+import os
+from config_functions import get_programdata_path, load_cfg
 
-load_dotenv()
-
-# get user programdata path
-import platform
-from pathlib import Path
-
-def get_programdata_path(appname: str = None) -> Path:
-    system = platform.system()
-
-    if system == "Windows":
-        base = Path(environ.get("PROGRAMDATA", "C:\\ProgramData"))
-    elif system == "Darwin":  # macOS
-        # Usa a pasta do usuário, não do sistema!
-        base = Path.home() / "Library/Application Support"
-    else:  # Linux
-        base = Path.home() / ".local/share"
-
-    return base / appname if appname else base
-
-#endregion
-
-# register or load our program data
 app_path = get_programdata_path("ProdDash")
-cfg_file_path = app_path / "app.cfg"
-username = getuser()
-
+cfg_file_path = app_path / "app_cfg.json" # json
 app_path.mkdir(parents=True, exist_ok=True)
 
-if not cfg_file_path.exists():
-    with open(cfg_file_path, "w") as file:
-        file.write(f"user={username}\n")
-        file.write(f"db_path=sqlite:///{(app_path / f'proddash_user_{username}.db')}\n")
-
-# load user program data
-def load_cfg_file(path: Path) -> dict:
-    data = {}
-    if path.exists():
-        with open(path) as f:
-            for line in f:
-                if "=" in line:
-                    key, value = line.strip().split("=", 1)
-                    data[key] = value
-    return data
-#endregion
-
-config_data = load_cfg_file(cfg_file_path)
-# print(config_data)
+cfg = load_cfg(cfg_file_path)
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
 class DevelopmentCfg(Config):
-    SQLALCHEMY_DATABASE_URI = config_data.get("db_path")
+    SQLALCHEMY_DATABASE_URI = cfg["db_uri"]
 
 class ProductionCfg(Config):
     pass
--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 from app import create_app
 from app.extensions import db
 from socketserver import TCPServer
-import os
+from config_functions import get_programdata_path, get_db_path, add_cfg, edit_cfg
+
+app_path = get_programdata_path("ProdDash")
+cfg_file_path = app_path / "app_cfg.json"
+app_path.mkdir(parents=True, exist_ok=True)
+db_path = get_db_path(app_path)
+
+add_cfg(cfg_file_path, {
+    "db_uri": db_path,
+    "port": 0
+})
 
 if __name__ == "__main__":
     app = create_app()
@@ -10,12 +20,13 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    # set a valid port
+    # get port
     with TCPServer(("localhost", 0), None) as s:
         free_port = s.server_address[1]
-        os.environ["PRODDASH_BACKEND_LOCALHOST_PORT"] = str(free_port)
-    
-    #int(os.environ["PRODDASH_BACKEND_LOCALHOST_PORT"]) 
-    port = 6769 
+        
+    edit_cfg(cfg_file_path, { "port": free_port })
 
-    app.run(debug=True, port=port, host="localhost")
+    app.run(debug=True, port=free_port, host="localhost")
+
+# how to access port in js
+# ./test/conn.js
