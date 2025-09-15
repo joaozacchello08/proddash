79a58036d5ed74ed8743cb56c1f5ce8e709f440d
João Carlos Castro Zacchello <joaozacchello@xongs-macbook-air.local>
2025-09-15 03:14:53 -0300

    .

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import os
 from get_sqlite_uri import get_sqlite_uri
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
 
 class DevelopmentCfg(Config):
-    SQLALCHEMY_DATABASE_URI = get_sqlite_uri()
+    # SQLALCHEMY_DATABASE_URI = get_sqlite_uri()
+    SQLALCHEMY_DATABASE_URI = os.getenv("SUPABASE_URI")
 
 class ProductionCfg(Config):
     pass
