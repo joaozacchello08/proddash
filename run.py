2439c92d6aaece7acabc57e06fc8f3ffaab3f5e9
João Carlos Castro Zacchello <joaozacchello@xongs-macbook-air.local>
2025-09-15 03:35:46 -0300

    .

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
 from app import create_app
 from app.extensions import db
 
-if __name__ == "__main__":
-    app = create_app()
 
-    # db
-    with app.app_context():
-        # db.drop_all()
-        db.create_all()
+app = create_app()
 
-    # app.run(debug=True, port=6969, host="localhost")
+with app.app_context():
+    # db.drop_all()
+    db.create_all()
 
-    app.run()    
+# if __name__ == "__main__":
+#     app = create_app()
+
+#     with app.app_context():
+#         # db.drop_all()
+#         db.create_all()
+
+#     app.run(debug=True, host="localhost", port=6969)
