f119329ed45420aa0352685ace9344be11e602e8
João Carlos Castro Zacchello <joaozacchello@xongs-macbook-air.local>
2025-09-15 03:24:01 -0300

    .

--- run.py
+++ run.py
@@ -9,4 +9,6 @@ if __name__ == "__main__":
         # db.drop_all()
         db.create_all()
 
-    app.run(debug=True, port=6969, host="localhost")
+    # app.run(debug=True, port=6969, host="localhost")
+
+    app.run()    
